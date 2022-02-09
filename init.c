cd3437037c7c184402a2965d088589842403356a
sorikikikim <sorikim98@naver.com>
2022-02-09 14:27:18 +0900

    fixed norm

--- init.c
+++ init.c
@@ -19,7 +19,7 @@ void	init_philo(t_philo *philo, t_state *state)
 
 int	init_fork(t_state *state)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (i < state->nb)
@@ -30,10 +30,10 @@ int	init_fork(t_state *state)
 	}
 	return (1);
 }
+
 int	init(t_state *state, char **argv, int argc)
 {
 	state->nb = ft_atoi(argv[1]);
-
 	if (state->nb > 300 || state->nb <= 0)
 		return (0);
 	state->time_die = ft_atoi(argv[2]);
@@ -43,7 +43,7 @@ int	init(t_state *state, char **argv, int argc)
 		state->must_eat = ft_atoi(argv[5]);
 	state->died = 0;
 	if (pthread_mutex_init(&state->write, NULL))
-		return (0);  
+		return (0);
 	init_philo(state->philos, state);
 	if (!init_fork(state))
 		return (0);
